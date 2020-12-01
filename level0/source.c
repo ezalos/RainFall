cc7a6775d909ce9736b494249df9653e569e10bd
Romain Kirszbaum <rkirszba@student.42.fr>
2020-12-01 13:07:47 +0100

    level0

---
+++ level0/source.c
@@ -0,0 +1,25 @@
+#include <unistd.h>
+#include <string.h>
+#include <stdio.h>
+
+int	main(int ac, char **av)
+{
+	char	*argv[2];
+	uid_t	euid;
+	gid_t	egid;
+
+	(void)ac;
+	if (atoi(av[1]) == 0x1a7)
+	{
+		argv[0] = strdup("/bin/sh");
+		argv[1] = 0;
+		egid = getegid();
+		euid = geteuid();
+		setresgid(egid, egid, egid);
+		setresuid(euid, euid, euid);
+		execv("/bin/sh", argv);
+	}
+	else
+		fwrite("No !\n", 1, 5, stderr);
+	return (0);
+}
