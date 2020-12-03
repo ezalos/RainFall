a035be8f8122b1dfeb3ca44a0dd56ba63acaa6e0
Romain Kirszbaum <rkirszba@student.42.fr>
2020-12-03 19:57:28 +0100

    beginning level6

---
+++ level6/source.c
@@ -0,0 +1,26 @@
+#include <string.h>
+#include <stdlib.h>
+
+void	m(void)
+{
+	puts("Nope");
+}
+
+void	n(void)
+{
+	system("/bin/cat /home/user/level7/.pass");
+}
+
+void	main(int ac, char **av)
+{
+	void	*ptr1;
+	void	(**ptr2)(void);
+
+	ptr1 = malloc(0x40);
+	ptr2 = malloc(0x4);
+	*ptr2 = m;
+	strcpy(ptr1, av[1]);
+	(*ptr2)();
+}
+
+// gcc -fno-stack-protector source.c -o source
