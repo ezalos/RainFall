165b22cb5dce0f553e39fc34ccbce9bd2d7cab11
Romain Kirszbaum <rkirszba@student.42.fr>
2020-12-14 15:24:12 +0100

    level9 cpp constructor ok

--- level9/source.cpp
+++ level9/source.cpp
@@ -1,65 +1,53 @@
 #include <string.h>
-#include <stdlib.h>
-// #include <new>
+#include <unistd.h>
 
-using namespace std;
+class Nb;
+
+int			operator+(Nb a, Nb b);
+int			operator-(Nb a, Nb b);
+
+// void		*g_fun_tab[2] = {&operator+, &operator-};
+
+int	(*g_funtab[2])(Nb, Nb) = {&operator+, &operator-};
+
+
 class Nb
 {
-	char	buf[0x64];
-	int		value;
 
 	public:
+		int		(**fun_tab)(Nb, Nb);
+		char	buf[0x64];
+		int		value;
+
 		Nb(int a)
 		{
-	        this ->value = a;
+			fun_tab = g_funtab;
+	        value = a;
 		}
 
-        void *operator new(size_t size)
-        {
-            void *p = malloc(size); 
-            return p;
-        }
-		void	operator+=(Nb a)
-		{
-			this ->value += a.value;
-		}
 		void	setAnnotation(char *str)
 		{
-			memcpy(this ->buf, str, strlen(str));
+			memcpy(buf, str, strlen(str));
 		}
 };
 
+int		operator+(Nb a, Nb b)
+{
+	return (a.value + b.value);
+}
 
-// Nb operator+(Nb a, Nb b)
-// {
-// 	return (Nb(a.value + b.value));
-// }
-
-// void	Nb::operator+=(Nb a)
-// {
-// 	value += a.value;
-// }
-
-// void	*Nb::operator new(size_t stAllocateBlock, int val)
-// {
-// 	Nb	*a;
-// 	a = (Nb*)malloc(stAllocateBlock);
-// 	a->value = val;
-// 	return (a);
-// }
-
-// void	Nb::setAnnotation(char *str)
-// {
-// 	memcpy(buf, str, strlen(str));
-// }
-
+int		operator-(Nb a, Nb b)
+{
+	return (a.value - b.value);
+}
 
 int		main(int ac, char **av)
 {
+	if (ac < 2)
+		_exit(1);
+
 	Nb	*a = new Nb(5);
 	Nb	*b = new Nb(6);
-
 	a->setAnnotation(av[1]);
-	*b += *a;
-	return (0);
+	return (((b->fun_tab)[0] )(*a, *b));
 }
