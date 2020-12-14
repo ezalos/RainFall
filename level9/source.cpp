691e01d3b8dad7da87f199f45bad43cb722a9577
ezalos <ezalos@gmail.com>
2020-12-14 13:44:52 +0100

    lvl9 cpp

--- level9/source.cpp
+++ level9/source.cpp
@@ -1,34 +1,44 @@
 #include <string.h>
 #include <stdlib.h>
-#include <new>
+// #include <new>
 
+using namespace std;
 class Nb
 {
-	public:
 	char	buf[0x64];
 	int		value;
 
-	Nb(int);
-	void	operator+=(Nb a);
-	void	setAnnotation(char *str);
+	public:
+		Nb(int a)
+		{
+	        this ->value = a;
+		}
 
+        void *operator new(size_t size)
+        {
+            void *p = malloc(size); 
+            return p;
+        }
+		void	operator+=(Nb a)
+		{
+			this ->value += a.value;
+		}
+		void	setAnnotation(char *str)
+		{
+			memcpy(this ->buf, str, strlen(str));
+		}
 };
 
-
-Nb::Nb(int val)
-{
-	value = val;
-}
 
 // Nb operator+(Nb a, Nb b)
 // {
 // 	return (Nb(a.value + b.value));
 // }
 
-void	Nb::operator+=(Nb a)
-{
-	value += a.value;
-}
+// void	Nb::operator+=(Nb a)
+// {
+// 	value += a.value;
+// }
 
 // void	*Nb::operator new(size_t stAllocateBlock, int val)
 // {
@@ -38,22 +48,18 @@ void	Nb::operator+=(Nb a)
 // 	return (a);
 // }
 
-void	Nb::setAnnotation(char *str)
-{
-	memcpy(buf, str, strlen(str));
-}
+// void	Nb::setAnnotation(char *str)
+// {
+// 	memcpy(buf, str, strlen(str));
+// }
 
 
 int		main(int ac, char **av)
 {
-	Nb	*a;
-	Nb	*b;
-	
-	a = new Nb(5);
-	b = new Nb(6);
+	Nb	*a = new Nb(5);
+	Nb	*b = new Nb(6);
 
 	a->setAnnotation(av[1]);
 	*b += *a;
 	return (0);
 }
-
